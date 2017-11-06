9f476003be3ec242ba4759f80e3084caa538ac4d
Chris Baker <electro@1939worldsfair.com>
2017-11-06 07:19:59 -0800

    bumped up logging

--- config/prod.exs
+++ config/prod.exs
@@ -19,7 +19,8 @@ config :weather_station, WeatherStationWeb.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
-config :logger, level: :info
+# config :logger, level: :info
+config :logger, level: :debug
 
 # ## SSL Support
 #
